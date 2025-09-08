c218a7599a4d6877d8c0b87478d5505780a83396
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-08 21:22:52 +0530

    typecasting

---
+++ Casting.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		double  d1= 12.345;
+		int i = (int)d1;   //size of double is greater than int
+		System.out.println(i);
+		//explicit 
+		//convert double into int
+		int d=34;
+		double q=(double)d;
+		System.out.println(d);
+		System.out.println(q);
+		//implicit
+	}
+
+}
