1ab152d5cfb47c80ce9410e95412cfcb7bb01feb
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-08 21:03:05 +0530

    Primitive datatype

---
+++ Datatype.java
@@ -0,0 +1,33 @@
+package day2;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		int aditya=21;
+		System.out.println(aditya);
+		
+		
+		float kg=23.34f;
+		System.out.println(kg);
+		
+		long bankbalance=1234567890;
+		System.out.println(bankbalance);
+		
+		char gender = 'm';
+		System.out.println(gender);
+		
+		String name = "aditya gaikwad";
+		System.out.println(name);
+		
+		System.out.println(aditya + kg);
+		
+		System.out.println(aditya+bankbalance+gender);
+		
+		System.out.println(bankbalance+gender);
+		
+		System.out.println(gender+name);
+		
+	
+
+	}
+}
