1c7d7d22af23bb6ee9c682cb520f5267a10c624f
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-08 21:03:55 +0530

    Primitive1 datatype

---
+++ Primitive.java
@@ -0,0 +1,30 @@
+package day2;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		int a = 30;
+		int b = 54;
+		System.out.println("my age is :" +a);
+		
+		System.out.println("my weight is:" +b);
+	
+		boolean a1 = true;
+		boolean a2=false;
+		
+			System.out.println("yash is mandbadak:" +a1);
+			System.out.println("yash is goodboy:"+a2);
+			
+		char name='%';
+				System.out.println(name);
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
