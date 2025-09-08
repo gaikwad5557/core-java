8dacd979c399370f5f47c9a72cb7a76844ac2616
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-08 21:18:48 +0530

    nonprimitive datatype

---
+++ Nonprimitive.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Nonprimitive {
+
+	public static void main(String[] args) {
+		int[] arr= new int[3];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		
+		System.out.println(arr[2]);
+		System.out.println(arr.length);//to print length of array use name.legth
+		
+		String[] str= new String[3];
+		str[0]="abc";
+		str[1]="234";
+		str[2]="aditya";
+		
+		System.out.println(str[1]);
+		System.out.println(str.length);
+
+	}
+
+}
