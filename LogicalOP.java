0131c048ef506b34e377a3c53203081e33965e66
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-09 12:15:26 +0530

    && Operator

---
+++ LogicalOP.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class LogicalOP {
+
+	public static void main(String[] args) {
+		System.out.println(true&&false);
+		System.out.println(false&&true);
+		System.out.println(false&&false);
+		System.out.println(true&&true);  //both true then true otherwise false
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
