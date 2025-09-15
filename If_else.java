b7d40cfee19333a5018c6272aae5e9375d54881c
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-15 23:21:11 +0530

    if else

---
+++ If_else.java
@@ -0,0 +1,33 @@
+package day3;
+
+public class If_else {
+
+	public static void main(String[] args) {
+		int age =14;
+		if (age >=18 )
+		{
+			System.out.println("vote is valid");
+			
+		}
+		else
+		{
+			System.out.println("vote is invalid");
+		}
+		String name ="aditya";
+		String password = "aditya@123";
+		
+		if (name.equals("aditya") && password.equals("aditya@123"))
+			
+		{
+			System.out.println("login succesfull");
+			
+		}
+		else
+		{
+			System.out.println("login failed");
+			
+		}
+				
+	}
+
+}
