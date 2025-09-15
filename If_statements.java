190508a5b6913f358ffe6ff5ccb67f92d0d541b6
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-15 23:20:10 +0530

    If statement

---
+++ If_statements.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class If_statements {
+
+	public static void main(String[] args) {
+		int age = 43;
+		if (age >=18 && age <= 80)
+		{
+			System.out.println("a is greater then 18 and a is less than 80");
+		}
+
+	}
+
+}
