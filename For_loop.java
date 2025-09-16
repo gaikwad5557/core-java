00cfcf328fdace1f4c4cafb14e56e7427957818a
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-16 12:36:05 +0530

    even odd

---
+++ For_loop.java
@@ -0,0 +1,30 @@
+package day3;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		for (int i = 0 ; i<5 ; i++)
+		{
+			System.out.println("hii");
+			
+		}
+
+		
+		
+		//even odd
+		
+		for (int i=0; i<20; i++)
+		{
+			if (i%2==0)
+			{
+				System.out.println("even:"+ i);
+				
+			}
+			else
+			{
+				System.out.println("no. is odd:"+i);
+			}
+		}
+	}
+
+}
