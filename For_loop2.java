d0705fc8b152f799bb0cbaee75d564f38d7dff9f
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-16 12:38:40 +0530

    divide by 13.5

---
+++ For_loop2.java
@@ -0,0 +1,34 @@
+package day3;
+
+public class For_loop2 {
+
+	public static void main(String[] args) {
+		
+		int sum=0;                           //sum of no
+		for (int i=0; i<50; i++) {
+			
+				sum=sum+i;
+				System.out.println("sumation is:"+sum);
+		
+		}
+		System.out.println("*****");	
+        System.out.println("sumation is:"+sum);
+	
+   
+	
+	
+	// divide by 13.5
+	
+	
+	for (double d = 0; d<100; d++)
+	{
+		if (d % 13.5==0)
+		{
+			System.out.println("divided by d:" +d);
+		}
+		
+		
+		
+}
+	}
+      }
