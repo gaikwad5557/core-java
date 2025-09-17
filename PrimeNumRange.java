c413a0f8d59d4a9964ddc62217af8ff9684f593a
gaikwad5557 <adityagaikwad5557@gmail.com>
2025-09-17 21:53:52 +0530

    prime numbers from 1 to 10

---
+++ PrimeNumRange.java
@@ -0,0 +1,35 @@
+package day3;
+
+public class PrimeNumRange {
+
+	public static void main(String[] args) {
+	
+		for (int i=2;  i<=100; i++)
+		{
+		boolean flag=true;
+		{
+			for(int j=2; j<i; j++)
+			{
+				if (i%j==0)
+				{
+					flag=false;
+					
+				}
+			}
+		
+		if(flag==true)
+		{
+			System.out.println(i+"is prime");
+			
+		}
+		else
+		{
+			System.out.println(i+"is not prime");
+		}
+
+	}
+
+}
+}
+
+}
